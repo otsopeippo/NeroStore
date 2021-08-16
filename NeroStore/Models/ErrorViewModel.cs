429ec0bdabc35413a5ab34b29c3f4aec3c52fcdd
o.peippo@outlook.com <o.peippo@outlook.com>
2021-08-16 10:59:21 +0300

    Add project files.

---
+++ NeroStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NeroStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
